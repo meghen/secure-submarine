f621fd72ed253cedbf63650a100281a95d006055
Shawn Miranda <theshawnmiranda@gmail.com>
2020-03-12 14:10:00 -0500

    changes

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    console.log('is authenticated?', req.isAuthenticated());
+    if(req.isAuthenticated()){
+        pool.query('SELECT * FROM "secret";')
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    }else{
+        res.sendStatus(403);
+    }
+   
 });
 
 module.exports = router;
